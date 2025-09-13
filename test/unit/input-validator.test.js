2f0e507f19eea145dd8cd06a5a24b601978998fa
berenif <bereni_f@etna-alternance.net>
2025-09-13 20:29:07 +0200

    test: ensure suspicious pattern detection is consistent

---
+++ test/unit/input-validator.test.js
@@ -0,0 +1,10 @@
+import { expect } from 'chai';
+import { inputValidator } from '../../src/utils/input-validator.js';
+
+describe('InputValidator suspicious pattern detection', () => {
+  it('should consistently detect directory traversal attempts', () => {
+    expect(inputValidator.containsSuspiciousPattern('../')).to.be.true;
+    expect(inputValidator.containsSuspiciousPattern('../')).to.be.true;
+    expect(inputValidator.containsSuspiciousPattern('../')).to.be.true;
+  });
+});
