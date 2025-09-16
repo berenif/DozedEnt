4d8d6b8bc5a9218e5d2a2a3b66ab8cf48e24e1f7
berenif <bereni_f@etna-alternance.net>
2025-09-16 10:51:47 +0200

    Fix utils exports and add persistent player identity

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,7 @@
-export {getRelaySockets, joinRoom, selfId} from './nostr.js'
+export {getRelaySockets, joinRoom, selfId} from '../netcode/nostr.js'
 export {default as RoomManager} from './room-manager.js'
-export {default as HostAuthority} from './host-authority.js'
-export {default as RoomLobbyUI} from './room-lobby-ui.js'
+export {default as HostAuthority} from '../host-authority.js'
+export {default as RoomLobbyUI} from '../netcode/room-lobby-ui.js'
 
 // Visual & Animation Features
 export {
@@ -12,7 +12,7 @@ export {
   CharacterAnimator,
   AnimationPresets,
   default as AnimationSystem
-} from './animation-system.js'
+} from '../animation/animation-system.js'
 export {default as CameraEffects} from './camera-effects.js'
 export {
   Particle,
@@ -37,16 +37,16 @@ export {
 export {
   EnhancedLobbyUI,
   default as EnhancedLobbyUIDefault
-} from './enhanced-lobby-ui.js'
+} from '../netcode/enhanced-lobby-ui.js'
 export {
   LobbyAnalytics,
   default as LobbyAnalyticsDefault
-} from './utils/lobby-analytics.js'
+} from './lobby-analytics.js'
 
 // Rollback Netcode exports
-export {default as RollbackNetcode} from './rollback-netcode.js'
-export {default as RollbackP2P} from './rollback-p2p.js'
-export {RollbackLobby, HOSTING_MODE} from './rollback-lobby.js'
+export {default as RollbackNetcode} from '../netcode/rollback-netcode.js'
+export {default as RollbackP2P} from '../netcode/rollback-p2p.js'
+export {RollbackLobby, HOSTING_MODE} from '../netcode/rollback-lobby.js'
 export {
   DeterministicGame,
   DeterministicRandom,
@@ -59,4 +59,4 @@ export {
   fixedSqrt,
   fixedSin,
   fixedCos
-} from './deterministic-game.js'
+} from '../netcode/deterministic-game.js'
